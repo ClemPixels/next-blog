e41c58715a81039a88bb4fd5f35f0b119c6eb2ac
Clement <kwasiclement764@gmail.com>
2025-05-31 23:56:01 +0000

    Update Header component button height for better accessibility

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -57,7 +57,7 @@ export default function Header() {
       </Button>
       <div className="flex gap-2 md:order-2">
         <Button
-          className="w-12 h-10 hidden sm:inline"
+          className="w-12 h-12 hidden sm:inline"
           color="gray"
           pill
           onClick={() => setTheme(theme === "light" ? "dark" : "light")}
